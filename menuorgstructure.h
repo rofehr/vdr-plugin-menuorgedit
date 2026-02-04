d0a7894fa66054ccb6a96ad5422d9e93f28f2153
rofehr <roland@diefehrs.de>
2026-02-04 16:21:35 +0100

    Changes

--- menuorgstructure.h
+++ menuorgstructure.h
@@ -42,6 +42,8 @@ public:
   void SetExecute(const char *Exec) { execute = Exec ? Exec : ""; }
   void SetConfirmExec(bool Confirm) { confirmExec = Confirm; }
   int Index(void) const;
+  cMenuorgItem *Prev(void) const { return prev; }
+  cMenuorgItem *Next(void) const { return next; }
 
   // Submenu management
   bool HasSubItems(void) const { return subItems != NULL; }
